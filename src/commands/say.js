b345ec2d04af00d7f89ab769e73f26fc64071739
Patricio Pittana <pittanapatricio@gmail.com>
2024-09-23 14:38:47 -0300

    Updated max characters of say command

--- src/commands/say.js
+++ src/commands/say.js
@@ -2,17 +2,17 @@ import Discord from 'discord.js';
 
 export default {
   data: new Discord.SlashCommandBuilder()
-    .setName('say') // Nombre del comando
-    .setDescription('El bot escribira tu mensaje') // Descripcion del comando
+    .setName('say')
+    .setDescription('El bot escribira tu mensaje')
     .addStringOption((option) =>
       option
         .setName('mensaje')
         .setDescription('El mensaje que quieres que escriba el bot')
         .setMinLength(3)
-        .setMaxLength(100)
+        .setMaxLength(300)
         .setRequired(true)
     ),
-  execute: async (interaction) => { // Funcion que se ejecuta cuando se usa el comando
+  execute: async (interaction) => {
     const message = interaction.options.getString('mensaje');
     await interaction.reply({ content: `${message}`, ephemeral: true }).catch(console.error);
   },
